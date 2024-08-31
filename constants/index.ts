ace78b78da2300e704cb86a2f158389893f33453
Andy Chang <yuhaoz1991@gmail.com>
2024-08-31 09:03:47 -0700

    Add side bar route pages and fix side bar hightlight select

--- constants/index.ts
+++ constants/index.ts
@@ -6,7 +6,7 @@ export const sidebarLinks = [
     },
     {
         label: 'Upcoming',
-        route: '/Upcoming',
+        route: '/upcoming',
         imgUrl: '/icons/upcoming.svg',
     },
     {
@@ -20,7 +20,7 @@ export const sidebarLinks = [
         imgUrl: '/icons/Video.svg',
     },
     {
-        label: 'Personal',
+        label: 'Personal Room',
         route: '/personal-room',
         imgUrl: '/icons/add-personal.svg',
     },
