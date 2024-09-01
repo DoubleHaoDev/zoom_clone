1c00f6af6838defa67b5162f9f79ab77e4ac7bc8
Andy Chang <yuhaoz1991@gmail.com>
2024-09-01 14:50:59 -0700

    Add modal to start video conferencing

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -2,18 +2,23 @@
 import React, {useState} from 'react';
 import HomeCard from "@/components/HomeCard";
 import {useRouter} from "next/navigation";
+import MeetingModal from "@/components/MeetingModal";
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState]
         = useState<"isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined >();
+
+    const createMeeting = () => {
+
+    }
     return (
         <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
             <HomeCard
                 img="/icons/add-meeting.svg"
                 title="New Meeting"
                 description="Start an instant meeting"
-                handleClick={() => setMeetingState("isJoiningMeeting")}
+                handleClick={() => setMeetingState("isInstantMeeting")}
                 className="bg-orange-1"
             />
             <HomeCard
@@ -36,6 +41,14 @@ const MeetingTypeList = () => {
                 description="via invitation link"
                 handleClick={() => setMeetingState("isJoiningMeeting")}
                 className="bg-yellow-1"
+            />
+            <MeetingModal
+                isOpen={meetingState === "isInstantMeeting"}
+                onClose={() => setMeetingState(undefined)}
+                title="Start an instant meeting"
+                className="text-center"
+                buttonText="Start Meeting"
+                handleClick={createMeeting}
             />
         </section>
     );
